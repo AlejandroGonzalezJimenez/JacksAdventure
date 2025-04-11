2b5fe07f87c569eef5de06d21e2149e902048a9e
Alejandro González Jiménez <alex63696@gmail.com>
2025-04-11 12:42:28 +0200

    Coins Counter, Lives Counter and Chest

--- Scripts/Treasures/coin.gd
+++ Scripts/Treasures/coin.gd
@@ -1,14 +1,8 @@
-extends RigidBody2D
+extends Treasure
 
 @export var _value : int = 1
-@onready var _sprite : AnimatedSprite2D = $AnimatedSprite2D
-@onready var _sfx : AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-func _on_body_entered(body: Node) -> void:
-	_sfx.play()
-	if not body is Character:
-		return
-	collision_mask = 0
-	_sprite.play("effect")
-	await _sprite.animation_finished
-	queue_free()
+func _collect():
+	$/root/Game.collect_coin(_value)
+	call_deferred("set_freeze_enabled", true)
+	call_deferred("set_freeze_mode", RigidBody2D.FREEZE_MODE_STATIC)
--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -3,12 +3,35 @@ extends Node2D
 @onready var _camera : Camera2D = $Camera2D
 @onready var _player_character : CharacterBody2D = $Jack
 @onready var _level : Area2D = $Level
+@onready var _coin_counter : Control = $UserInterface/CoinCounter
+@onready var _lives_counter : Control = $UserInterface/LivesCounter
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_init_boundaries()
+	_init_ui()
+
+func _init_boundaries():
 	# get the level boundaries from the level
 	var min_boundary : Vector2 = _level.get_min()
 	var max_boundary : Vector2 = _level.get_max()
 	# and tell them to the camera and player character
 	_camera.set_bounds(min_boundary, max_boundary)
 	_player_character.set_bounds(min_boundary, max_boundary)
+
+func _init_ui():
+	#initialize the UI
+	_coin_counter.set_value(File.data.coins)
+	_lives_counter.set_value(File.data.lives)
+
+func collect_coin(value : int):
+	File.data.coins += value
+	if File.data.coins >= 100:
+		File.data.coins -= 100
+		File.data.lives += 1
+		_lives_counter.set_value(File.data.lives)
+	_coin_counter.set_value(File.data.coins)
+
+func collect_skull():
+	File.data.lives += 1
+	_lives_counter.set_value(File.data.lives)
